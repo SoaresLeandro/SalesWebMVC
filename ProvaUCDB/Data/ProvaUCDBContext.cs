a792557f95a999107e65a7d36aedd7c3ffe94aac
Soares Leandro <lss.91@live.com>
2021-04-10 20:42:56 -0400

    Criação do CRUD de  Produtos

---
+++ ProvaUCDB/Data/ProvaUCDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProvaUCDB.Models;
+
+namespace ProvaUCDB.Data
+{
+    public class ProvaUCDBContext : DbContext
+    {
+        public ProvaUCDBContext (DbContextOptions<ProvaUCDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProvaUCDB.Models.Product> Product { get; set; }
+    }
+}
--- ProvaUCDB/Startup.cs
+++ ProvaUCDB/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProvaUCDB.Data;
 
 namespace ProvaUCDB
 {
@@ -33,6 +35,9 @@ namespace ProvaUCDB
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProvaUCDBContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProvaUCDBContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
