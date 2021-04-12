8362b6a7f28b7e64a891e49b05ee8326d95d559f
Soares Leandro <lss.91@live.com>
2021-04-12 07:22:09 -0400

    CRUD básico de "Order"

---
+++ ProvaPraticaUCDB/Data/ProvaPraticaUCDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProvaPraticaUCDB.Models;
+
+namespace ProvaPraticaUCDB.Data
+{
+    public class ProvaPraticaUCDBContext : DbContext
+    {
+        public ProvaPraticaUCDBContext (DbContextOptions<ProvaPraticaUCDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Order { get; set; }
+    }
+}
