6bea50ec96d4b0ebc6b14fe9db647d2413e3e072
Soares Leandro <lss.91@live.com>
2021-04-12 07:02:44 -0400

    Criação do Model "Order" e da Enumeração "OrderStatus"

---
+++ ProvaPraticaUCDB/Models/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaPraticaUCDB.Models.Enums
+{
+    public enum OrderStatus : int
+    {
+        Valid = 0,
+        CloseExpiration = 1,
+        Expired = 2
+    }
+}
---
+++ ProvaPraticaUCDB/Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProvaPraticaUCDB.Models.Enums;
+
+namespace ProvaPraticaUCDB.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string NameProduct { get; set; }
+        public double Value { get; set; }
+        public DateTime DueDate { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(int id, string nameProduct, double value, DateTime dueDate, OrderStatus status)
+        {
+            Id = id;
+            NameProduct = nameProduct;
+            Value = value;
+            DueDate = dueDate;
+            Status = status;
+        }
+
+        public void ApplyDiscount(double value)
+        {
+            if (DueDate <= DateTime.Now)
+            {
+                Value -= value;
+            }
+        }
+    }
+}
